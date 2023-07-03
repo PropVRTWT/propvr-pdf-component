1a583baa3414aad7536f377272ab13340f883a7e
sanjaykumar66 <sanjayselva59@gmail.com>
2023-07-03 12:53:27 +0530

    init

---
+++ src/components/PDFComp.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref,onMounted } from 'vue'
+const DFLip_container = ref();
+const props = defineProps({ 
+  url: String 
+})
+var options = {hard:'cover', pageMode: window.DFLIP.PAGE_MODE.SINGLE,webgl:false,height: 500, duration: 800,hideControls: "share,download,pageMode,startPage,endPage,sound"};
+
+onMounted(()=>{
+  DFLip_container.value.innerHTML = '';
+  $("#df_manual_book").flipBook(props.url, options);
+})
+</script>
+
+<template>
+    <div ref="DFLip_container" id="df_manual_book">
+	  </div>
+</template>
